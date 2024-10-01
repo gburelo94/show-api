3e4832f4b3af3e58c9aa17f9a364bd5eb6373c89
Gaby Burelo <gaby.burelo@outlook.com>
2024-09-30 20:30:16 -0600

    Inital commit

---
+++ src/main/java/com/gburelo/shows_api/ShowsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gburelo.shows_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gburelo/shows_api/ShowsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gburelo.shows_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
